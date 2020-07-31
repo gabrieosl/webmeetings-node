af8171e45c434bd8ed6b67602785b8ac8c38eb6e
gabrieosl <ga.bri.el@msn.com>
2020-07-31 12:41:06 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    console.log(roomId, userId);
+  });
+});
+
+server.listen(3000);
